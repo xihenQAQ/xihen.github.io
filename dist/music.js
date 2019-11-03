b66e6eedca982d9af541e3cd1853e87982b6893b
xihenQAQ <1528504214@qq.com>
2019-11-03 15:43:57 +0800

    Site updated: 2019-11-03 15:43:56

---
+++ dist/music.js
@@ -0,0 +1,10 @@
+const ap = new APlayer({
+    container: document.getElementById('player'),
+    fixed: true,
+    audio: [{
+        name: '红昭愿',
+        artist: '音阙诗听',
+        url: 'http://isure.stream.qqmusic.qq.com/C100002IWBxU47dwlj.m4a?fromtag=32.mp3',
+        cover: 'cover.jpg',
+    }]
+});
